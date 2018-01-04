98cd092144d6e3441e947255139b21f40ec96b9c
rockstar3595 <alunsford95@gmail.com>
2018-01-03 20:22:18 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,24 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const url = "https://api.coinmarketcap.com/v1/ticker/bitcoin/";
+
+const request = require('request');
+
 client.on('ready', () => {
       console.log('I am ready!');
       });
       
       client.on('message', message => {
-      if (message.content === 'ping'){
-      message.reply('pong');
+      if (message.content === 'bitcoin'){
+            
+            request.get(url, (error, response, body) => {
+			var json = JSON.parse(body);
+            
+            
+      message.reply('Coin: ${json[0]["id"]} \n Price: ${json[0]["price_usd"]}');
+      });
+                  
       }
       });
       
